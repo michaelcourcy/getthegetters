dbbbc1df19b7786dfee793bb9a856c8883b5f0d0
Michael Courcy <michael.courcy@gmail.com>
2013-12-15 16:36:58 +0100

    Adding the source code

---
+++ src/test/java/mic/prod/getthegetters/GetTheGettersTest.java
@@ -0,0 +1,46 @@
+package mic.prod.getthegetters;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GetTheGettersTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public GetTheGettersTest( String testName )
+    {
+        super( testName );
+    }
+
+    
+
+    /**
+     * @throws IntrospectionException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalAccessException 
+     */
+    public void testApp() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, IntrospectionException
+    {
+        Obj1 obj1 = new Obj1();
+        GetTheGetters getTheGetters = new GetTheGetters(new GetTheGetters.GetterHandler() {
+			
+			public void handle(String getter, Object value) {
+				System.out.println(getter + " : " + value);
+			}
+		});
+        getTheGetters.getTheGetters(obj1);
+        
+    }
+}
---
+++ src/test/java/mic/prod/getthegetters/Obj2.java
@@ -0,0 +1,36 @@
+package mic.prod.getthegetters;
+
+
+
+public class Obj2 {
+	private String pp3 = "pop3";
+	private String pp4 = "pop4";
+	public Obj2(String pp3, String pp4) {
+		super();
+		this.pp3 = pp3;
+		this.pp4 = pp4;
+	}
+	public String getPp3() {
+		return pp3;
+	}
+	public void setPp3(String pp3) {
+		this.pp3 = pp3;
+	}
+	public String getPp4() {
+		return pp4;
+	}
+	public void setPp4(String pp4) {
+		this.pp4 = pp4;
+	}
+	
+	private Obj3 obj3 = new Obj3("hop", "hup");
+	
+	public Obj3 getObj3() {
+		return obj3;
+	}
+	public void setObj3(Obj3 moiMeme) {
+		this.obj3 = moiMeme;
+	}
+			
+			
+}
